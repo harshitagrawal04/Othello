de9d216b353e5d2239fe7dd6f3c8388e78bd2fd3
Harshit Agrawal <harshitagrawal2004@gmail.com>
2025-09-04 15:35:48 +0530

    minor update
    checking for the legal moves and all the available legal moves for the current player

--- src/main.cpp
+++ src/main.cpp
@@ -18,47 +18,114 @@ char cellToChar(CellState cell) {
 }
 
 // setting all the cells to empty
-// void resetBoard(cellState[][])
-
-
-struct Move {
-    int row;
-    int col;
-};
-
-
-
-int main() {
-    CellState board[8][8];
-
+void resetBoard(CellState board[8][8]) {
     for (int row = 0; row < 8; row++) {
         for (int col = 0; col < 8; col++) {
             board[row][col] = CellState::Empty;
         }
     }
-
-
     board[3][3] = CellState::Black;
     board[4][4] = CellState::Black;
     board[3][4] = CellState::White;
     board[4][3] = CellState::White;
+}
 
-
+// display the board
+void displayBoard(CellState board[8][8]) {
     for (int row = 0; row < 8; row++) {
         for (int col = 0; col < 8; col++) {
             cout << cellToChar(board[row][col]) << " ";
         }
         cout << endl;
     }
+}
+
+struct Move {
+    int row;
+    int col;
+};
+
+// directions in which cells can flip
+int directions[8][2] = {
+    {-1, -1}, {-1, 0}, {-1, 1},   // up-left, up, up-right
+    { 0, -1},          { 0, 1},   // left,      right
+    { 1, -1}, { 1, 0}, { 1, 1}    // down-left, down, down-right
+};
+
+// checking if the given move is legal or not
+bool legalMove(Move move, CellState board[8][8], CellState currentPlayer) {
+    int row = move.row;
+    int col = move.col;
+
+    CellState opponent = (currentPlayer == CellState::White)
+                     ? CellState::Black
+                     : CellState::White;
+
+
+    if (board[row][col] != CellState::Empty) {
+        return false;
+    }
+    else {
+        for (auto& dir : directions) {
+            int dRow = dir[0];
+            int dCol = dir[1];
+
+            int newrow = row + dRow;
+            int newcol = col + dCol;
+
+            if (newrow >= 0 && newrow < 8 && newcol >= 0 && newcol < 8) {
+                if (board[newrow][newcol] == opponent) {
+                    newrow += dRow;
+                    newcol += dCol;
+                }
+                else {
+                    continue;
+                }
+            }
+            else {
+                continue;
+            }
+
+            while (newrow >= 0 && newrow < 8 && newcol >= 0 && newcol < 8) {
+                if (board[newrow][newcol] == currentPlayer) {
+                    return true;
+                }
+                newrow += dRow;
+                newcol += dCol;
+            }
+        }
+    }
+    return false;
+}
+
+// checking for all legal moves for the current player
+vector<Move> findAllLegalMoves(CellState board[8][8], CellState currentPlayer) {
+    vector<Move> moves;
+    for (int row = 0; row < 8; row++) {
+        for (int col = 0; col < 8; col++) {
+            if (legalMove({row, col}, board, currentPlayer)) {
+                moves.push_back({row, col});
+            }
+        }
+    }
+
+    return moves;
+}
+
+int main() {
+    CellState board[8][8];
+
+    resetBoard(board);
+
+    displayBoard(board);
 
     vector<Move> moves;
 
-    moves.push_back({0,0});
-    moves.push_back({0,1});
-    moves.push_back({1,0});
 
-    for (int i = 0; i < moves.size(); i++) {
-        cout << moves[i].row << " " << moves[i].col << endl;
+    moves = findAllLegalMoves(board, CellState::White);
+
+    for (auto& move : moves) {
+        cout << move.row << ", " << move.col << endl;
     }
 
 
