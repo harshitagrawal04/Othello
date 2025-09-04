95db34e36c7e0d54ff6dda7b3170daa5b40d0248
Harshit Agrawal <harshitagrawal2004@gmail.com>
2025-09-04 18:22:05 +0530

    minor update
    added a function which display the coordinate of all the legal moves, make the move and flip the cell in all direction if avaliable, basic game simulator to test the game

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,18 @@ enum class CellState {
     White
 };
 
+struct Move {
+    int row;
+    int col;
+};
+
+// directions in which cells can flip
+int directions[8][2] = {
+    {-1, -1}, {-1, 0}, {-1, 1},   // up-left, up, up-right
+    { 0, -1},          { 0, 1},   // left,      right
+    { 1, -1}, { 1, 0}, { 1, 1}    // down-left, down, down-right
+};
+
 // helper function to display the value of cell
 char cellToChar(CellState cell) {
     if (cell == CellState::Empty) return '.';
@@ -38,19 +50,16 @@ void displayBoard(CellState board[8][8]) {
         }
         cout << endl;
     }
+    cout << endl;
 }
 
-struct Move {
-    int row;
-    int col;
-};
-
-// directions in which cells can flip
-int directions[8][2] = {
-    {-1, -1}, {-1, 0}, {-1, 1},   // up-left, up, up-right
-    { 0, -1},          { 0, 1},   // left,      right
-    { 1, -1}, { 1, 0}, { 1, 1}    // down-left, down, down-right
-};
+// display the moves options for the player
+void displayMoves(const vector<Move>& moves = {}) {
+    for (auto& move : moves) {
+        cout << "(" << move.row << "," << move.col << ")\n";
+    }
+    cout << endl;
+}
 
 // checking if the given move is legal or not
 bool legalMove(Move move, CellState board[8][8], CellState currentPlayer) {
@@ -112,6 +121,44 @@ vector<Move> findAllLegalMoves(CellState board[8][8], CellState currentPlayer) {
     return moves;
 }
 
+// making the move and updating the board
+void makeMove(Move move, CellState board[8][8], CellState currentPlayer) {
+    if (!legalMove(move, board, currentPlayer)) return;
+
+    board[move.row][move.col] = currentPlayer;
+
+    CellState opponent = (currentPlayer == CellState::White)
+                 ? CellState::Black
+                 : CellState::White;
+
+    for (auto& dir : directions) {
+        int dRow = dir[0];
+        int dCol = dir[1];
+
+        int newrow = move.row + dRow;
+        int newcol = move.col + dCol;
+
+        vector<pair<int, int>> toFlip;
+
+        while (newrow >= 0 && newrow < 8 &&
+            newcol >= 0 && newcol < 8 &&
+            board[newrow][newcol] == opponent) {
+            toFlip.push_back({newrow, newcol});
+            newrow += dRow;
+            newcol += dCol;
+        }
+
+        if (!toFlip.empty() &&
+            newrow >= 0 && newrow < 8 &&
+            newcol >= 0 && newcol < 8 &&
+            board[newrow][newcol] == currentPlayer) {
+            for (auto& cell : toFlip) {
+                board[cell.first][cell.second] = currentPlayer;
+            }
+        }
+    }
+}
+
 int main() {
     CellState board[8][8];
 
@@ -119,13 +166,28 @@ int main() {
 
     displayBoard(board);
 
-    vector<Move> moves;
+    CellState currentPlayer = CellState::White;
 
+    while (!findAllLegalMoves(board, currentPlayer).empty()) {
+        vector<Move> moves = findAllLegalMoves(board, currentPlayer);
 
-    moves = findAllLegalMoves(board, CellState::White);
+        cout << "available moves: ";
+        displayMoves(moves);
 
-    for (auto& move : moves) {
-        cout << move.row << ", " << move.col << endl;
+        int r, c;
+        cout << "Enter your move (row and col): ";
+        cin >> r >> c;
+        Move move = {r,c};
+        if (legalMove(move, board, currentPlayer)) {
+            makeMove(move, board, currentPlayer);
+            displayBoard(board);
+            currentPlayer = (currentPlayer == CellState::White)
+             ? CellState::Black
+             : CellState::White;
+
+        } else {
+            cout << "Illegal move! Try again.\n";
+        }
     }
 
 
